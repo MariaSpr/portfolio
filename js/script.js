c54a278f2594ea109b1dff0bb2f7da0ccab3b52b
Maria Siapera <mariasiapera@outlook.com>
2017-03-16 13:38:07 +0200

    Add social bitbucket

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/* navbar active state*/
-
-// $('.nav li a').click(function(e) {
-//   var $this = $(this);
-//   if (!$this.hasClass('active_nav')) {
-//     $this.addClass('active_nav');
-//   }
-//   e.preventDefault();
-// });
